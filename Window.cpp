fa2eee21a1b1b94a4f9f7e768d6f631465fcd925
uranusjr <uranusjr@iMac.local>
2011-03-16 02:39:39 +0800

    Changed button layout from horizontal to grid
    4 buttons are now in 2x2 matrix, and Buttons 0 and 1 are in first COLUMN

--- Window.cpp
+++ Window.cpp
@@ -1,16 +1,16 @@
 #include "Window.h"
 #include <QtGui/QApplication>
-#include <QtGui/QHBoxLayout>
+#include <QtGui/QGridLayout>
 #include <QtGui/QPushButton>
 
 Window::Window(QWidget *parent) : QWidget(parent)
 {
-    QHBoxLayout *l = new QHBoxLayout;
+    QGridLayout *l = new QGridLayout;
 	for (unsigned int i = 0; i < 4; i++)
 	{
 	    QString buttonName = QString("Button ") + QString::number(i);
 	    QPushButton *b = new QPushButton(buttonName, this);
-	    l->addWidget(b);
+	    l->addWidget(b, i%2, i/2);
 	    buttons.append(b);
 	    connect(b, SIGNAL(clicked()), qApp, SLOT(quit()));
 	}
